e2a071f20802c2720eaaf38148e934a5fbc92a9d
Pierre Mathieu <pamathieu@hotmail.com>
2019-01-10 05:05:48 -0500

    First test for Debian package statistics

--- .idea/package_statistics.py
+++ .idea/package_statistics.py
@@ -1,4 +1,7 @@
-import urllib
+import urllib, os, os.path
 
 testfile = urllib.URLopener()
 testfile.retrieve("http://ftp.uk.debian.org/debian/dists/stable/main/Contents-udeb-arm64.gz", "filecontent.gz")
+
+DIR = '/tmp'
+print len([name for name in os.listdir(DIR) if os.path.isfile(os.path.join(DIR, name))])
